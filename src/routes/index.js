b277546f9bda6f77458990b03b85e04ac2be1696
BenDev181 <benjiro181@gmail.com>
2024-10-17 15:46:49 +0100

    Associations completed

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,9 +16,44 @@ userRouter.get("/", async (req, res) => {
     res.json(users)
 })
 
+userRouter.get("/:id", async (req, res) => {
+    const id = req.params.id
+    let user = await User.findByPk(id)
+    res.json(user)
+})
+
+userRouter.get("/:id/shows", async (req, res) => {
+    const id = req.params.id
+    let user = await User.findByPk(id, {
+        include: Show,
+    })
+    res.json(user)
+})
+
+
+
+
+
 
 // showRouter
+showRouter.get("/", async (req, res) => {
+    let shows = await Show.findAll()
+    res.json(shows)
+})
 
+showRouter.get("/:id", async (req, res) => {
+    const id = req.params.id
+    let show = await Show.findByPk(id)
+    res.json(show)
+})
+
+showRouter.get("/:id/users", async (req, res) => {
+    const id = req.params.id
+    let show = await Show.findByPk(id, {
+        include: User,
+    })
+    res.json(show)
+})
 
 
 
